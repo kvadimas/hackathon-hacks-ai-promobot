35ffcfd1fa897631a2fc6e78675cc3769473deec
Vadim Kurotkin <kvadimas1@gmail.com>
2024-01-02 01:43:05 +0300

    serverless fix

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 import timeit
 import logging
 import uvicorn
@@ -5,7 +6,6 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.templating import Jinja2Templates
-from fastapi_throttling import ThrottlingMiddleware
 
 
 from serializers import ForecastRequest, ForecastResponse
@@ -14,20 +14,11 @@ from model import forecast
 from ner import get_location
 
 
-def setup_logging():
-    app_logger = logging.getLogger(__name__)
-    app_logger.setLevel(logging.INFO)
-    app_handler = logging.StreamHandler()
-    app_formatter = logging.Formatter("%(name)s %(asctime)s %(levelname)s : %(message)s")
-    app_handler.setFormatter(app_formatter)
-    app_logger.addHandler(app_handler)
-
-
-setup_logging()
-_logger = logging.getLogger(__name__)
+logger = logging.getLogger('app')
+logger.setLevel(logging.DEBUG)
+logging.basicConfig(level=logging.DEBUG)
 
 app = FastAPI(version="1.0")
-app.add_middleware(ThrottlingMiddleware, limit=100, window=60)
 
 templates = Jinja2Templates(directory="templates")
 
@@ -35,6 +26,7 @@ templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     ''' Демо страница '''
+    logger.info(f'Start index.html')
     return templates.TemplateResponse("index.html", {"request": request})
 
 
@@ -42,20 +34,26 @@ async def read_root(request: Request):
 async def create_forecast(message: ForecastRequest):
     '''Запуск системы ml'''
     start_time = timeit.default_timer()
-    sub = forecast(message.text.strip())
-    group = department = 'Пока не работает'
-    location = get_location(message.text.strip())
-    result: dict = {
-        'group': group,
-        'sub': sub,
-        'location': location,
-        'department': department,
-        'text': message.text.strip(),
-    }
-    elapsed_time = timeit.default_timer() - start_time
-    _logger.info(f'Elapsed time: {elapsed_time}')
-    return JSONResponse(content=result, status_code=200)
+    logger.debug(f'Message: {message.text.strip()}')
+    try:
+        sub = forecast(message.text.strip())
+        group = department = 'Пока не работает'
+        location = get_location(message.text.strip())
+        result: dict = {
+            'group': group,
+            'sub': sub,
+            'location': location,
+            'department': department,
+            'text': message.text.strip(),
+        }
+        elapsed_time = timeit.default_timer() - start_time
+        logger.info(f'Elapsed time: {elapsed_time}')
+        return JSONResponse(content=result, status_code=200)
+    except:
+        logger.error(f'Error forecast')
+        return JSONResponse(content={"error": "error forecast"}, status_code=500)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=9000)
+    port = int(os.environ['VCAP_APP_PORT'])
+    uvicorn.run(app, host="0.0.0.0", port=port)
